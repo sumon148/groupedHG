ebe20168aee273658694abbd03ff0bb924d1c80a
Sumonkanti Das <u1107832@anu.edu.au>
2025-06-23 13:21:04 +1000

    Updated Readme file

--- R/Run Rmd Md file.R
+++ R/Run Rmd Md file.R
@@ -1,20 +1,13 @@
 # Run the Rmd and md file
 
 rmarkdown::render(
-  "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/NEWS.md",
+  "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/NEWS.Rmd",
   output_file = "NEWS.pdf",
   output_dir = "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG"
 )
 
 rmarkdown::render(
-  "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/README.md",
-  output_file = "README.pdf",
-  output_dir = "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG"
-)
-
-
-rmarkdown::render(
-  "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/README.Rmd",
-  output_file = "README.pdf",
+  "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/README_pdf_document.Rmd",
+  output_file = "README_pdf_document.pdf",
   output_dir = "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG"
 )
