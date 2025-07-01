b325d2c66c7d51a0f27e5007d5605da3c828794f
Mahdi Parsa <mahdiprs@gmail.com>
2025-07-01 13:22:34 +1000

    Update Run Rmd Md file.R
    comment out the local local file for rendering

--- R/Run Rmd Md file.R
+++ R/Run Rmd Md file.R
@@ -1,8 +1,8 @@
 # Run the Rmd and md file
 
-rmarkdown::render(
-  "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/NEWS.Rmd",
-  output_file = "NEWS.pdf",
-  output_dir = "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG"
-)
+# rmarkdown::render(
+#   "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG/NEWS.Rmd",
+#   output_file = "NEWS.pdf",
+#   output_dir = "C:/users/u1107832/OneDrive - Australian National University/R Package Development/groupedHG"
+# )
 
